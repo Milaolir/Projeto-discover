48a0bee93c2a7128d75fef644887f795ea33c2e2
Camila <camilaoramos7@gmail.com>
2023-06-27 10:29:49 -0300

    :tada:initial commit

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+const html = document.documentElement
+html.classList.toggle("light")
+ 
+//pegar a tag img
+const img = document.querySelector("#profile img")
+
+//subistituir a imagem
+if(html.classList.contains('light')) {
+  //se tiver light mode, add a imagem light
+
+  img.setAttribute ("src", "./assets/avatar-light.png")
+ } else {
+  //se tiver sem light mode, manter avatar normal
+  img.setAttribute("src", "./assets/avatar.png")
+}
+}
+
